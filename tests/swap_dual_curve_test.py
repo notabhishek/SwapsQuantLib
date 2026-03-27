1c83e593610404d2a4eff969ad39257a0b2138a7
Abhishek Tiwari <notabhishektiwari@gmail.com>
2026-03-27 14:01:36 +0000

    format swap test outputs

--- tests/swap_dual_curve_test.py
+++ tests/swap_dual_curve_test.py
@@ -13,7 +13,7 @@ swap = Swap(datetime(2022, 2, 14), tenor_m=4, period_fix_m=12, period_float_m=1)
 
 print(swap)
 
-print(f'par_rate={swap.rate(curve)}')
-print(f'{swap.analytic_delta(curve, notional=1e9)}')
-print(f'{swap.npv(curve, fixed_rate=1.15, notional=1e9)}')
+print(f'par_rate              = {swap.rate(curve)}')
+print(f'dv01 for 1e9 notional = {swap.analytic_delta(curve, notional=1e9)}')
+print(f'npv  for 1e9 notional = {swap.npv(curve, fixed_rate=1.15, notional=1e9)}')
 
