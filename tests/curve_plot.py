a3261ee3f100d33fcaf5f199f098d509324bf3c3
Abhishek Tiwari <notabhishektiwari@gmail.com>
2026-03-29 18:03:32 +0100

    plot overnight forward rates from interpolated dfs

--- tests/curve_plot.py
+++ tests/curve_plot.py
@@ -25,19 +25,40 @@ def generate_points(curve: Curve, start_date: datetime, end_date: datetime):
 
     x = np.array(dates)
     y = np.array(dfs)
-    return x, y 
+
+    # calculating forward O/N rates 
+    # DF(t) = e ^ integration(-fi . ti)
+    # ln(DF) = integration(-fi.ti)
+    # -d ln(DF)/dt = fi
+    log_df = np.log(y)
+    numerator = -(log_df[1:] - log_df[:-1])
+    denominator = 1 / 365 # ACT/365 DCF
+    
+    fwds = numerator / denominator
+    fwd_dates = x[:-1]
+
+    return x, y, fwd_dates, fwds
 
 start_date = datetime(2022, 1, 1)
 end_date = datetime(2022, 12, 31)
 
-x_log_linear, y_log_linear = generate_points(curve_log_linear, start_date, end_date)
-x_linear, y_linear = generate_points(curve_linear, start_date, end_date)
+x_log_linear, y_log_linear, fwd_d_log, fwds_log = generate_points(curve_log_linear, start_date, end_date)
+x_linear, y_linear, fwd_d_lin, fwds_lin = generate_points(curve_linear, start_date, end_date)
 
-plt.plot(x_log_linear, y_log_linear)
-plt.plot(x_linear, y_linear)
+plt.subplot(1, 2, 1)
+plt.title("Discount factor Vs date")
+plt.xlabel("Date(T)")
+plt.ylabel("DF(t0, T)")
+plt.plot(x_log_linear, y_log_linear, label='log_linear interpolation')
+plt.plot(x_linear, y_linear, label='linear interpolation')
+plt.legend()
 
-plt.title("Discount factor Curves")
-plt.xlabel("Date")
-plt.ylabel("DF")
+plt.subplot(1, 2, 2)
+plt.title("O/N forward rate curve")
+plt.xlabel("Date(T)")
+plt.ylabel("f(Ti, Ti+1)")
+plt.plot(fwd_d_log, fwds_log, label='log_linear interpolation')
+plt.plot(fwd_d_lin, fwds_lin, label='linear interpolation')
 
+plt.legend()
 plt.show()
