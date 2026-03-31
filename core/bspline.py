ffc4a4f7dbbe6de71befc91509ce1bcb89cbd28d
Abhishek Tiwari <notabhishektiwari@gmail.com>
2026-03-31 12:38:51 +0100

    implement BSpline

--- core/bspline.py
+++ core/bspline.py
@@ -8,6 +8,11 @@ Calculate Bi,k(x) for knot sequence t
     - endpoint support (Bi,k = 1 for i >= len(t)-1-k)
     - k >= 1
 """
+
+import numpy as np 
+import core.linalg as linalg 
+from datetime import timedelta
+
 def bsplev_single(x, i, k, t, org_k = None):
     # Endpoint support 
     org_k = org_k or k # number of times the last knot point is repeated
@@ -50,16 +55,79 @@ def bspldnev_single(x, i, k, t, n, org_k = None):
     org_k = org_k or k 
     res, div1, div2 = 0, t[i+k-1]-t[i], t[i+k]-t[i+1]    
 
+    if isinstance(div1, timedelta):
+        div1 = div1 / timedelta(days=1)
+    if isinstance(div2, timedelta):
+        div2 = div2 / timedelta(days=1)
+
     if n == 1:
         if div1 != 0:
             res += bsplev_single(x, i, k-1, t, org_k) / div1 
         if div2 != 0:
-            res += bsplev_single(x, i+1, k-1, t, org_k) / div2
+            res -= bsplev_single(x, i+1, k-1, t, org_k) / div2
         res *= (k-1)
     else:
         if div1 != 0:
             res += bspldnev_single(x, i, k-1, t, n-1, org_k) / div1 
         if div2 != 0:
-            res += bspldnev_single(x, i+1, k-1, t, n-1, org_k) / div2 
+            res -= bspldnev_single(x, i+1, k-1, t, n-1, org_k) / div2 
         res *= (k-1) 
     return res
+
+class BSpline: 
+    def __init__(self, k, t):
+        self.t, self.k, self.n = t, k, len(t) - k
+
+    # Evaluate Bi,k,t(x) for an array of x
+    def bsplev(self, x, i, otypes=['float64']):
+        func = np.vectorize(bsplev_single, excluded=['k', 't'], otypes=otypes) 
+        return func(x, i=i, k=self.k, t=self.t)
+    
+    # Evaluate dn/dx (Bi,k,t(x)) for an array of x
+    def bspldnev(self, x, i, n, otypes=['float64']):
+        func = np.vectorize(bspldnev_single, excluded=['k', 't'], otypes=otypes)
+        return func(x, i=i, k=self.k, t=self.t, n=n)
+    
+    """
+    build splien colocation matrix and solve 
+    
+    tau = T = observed market data inputs
+
+            [B0,k(T0)       B1,k(T0)    ...     Bz,k(T0)]
+    B_ji =  [B0,k(T1)       B1,k(T1)    ...     Bz,k(T1)]
+            [B0,k(T2)       B1,k(T2)    ...     Bz,k(T2)]
+
+    Except, at boundary conditions, 1st row and last row 
+    we want to be able to specify nth derivative of spline equals 
+    some expected value and solve for coefficients using that
+    """
+    def bsplmatrix(self, tau, left_n=0, right_n=0):
+        B_ji = np.zeros(shape=(len(tau), self.n))
+
+        for i in range(self.n):
+            B_ji[0,i] = bspldnev_single(tau[0], i, self.k, self.t, left_n)
+            B_ji[1:-1, i] = self.bsplev(tau[1:-1], i=i)
+            B_ji[-1,i] = bspldnev_single(tau[-1], i, self.k, self.t, right_n)
+        return B_ji
+    
+    def bsplsolve(self, tau, y, left_n, right_n):
+        """Evaluate the B-Spline coeffs `c` that parametrise the pp."""
+        if len(tau) != self.n:
+            raise ValueError(f"`tau` must have length equal to pp dimension, "
+                             f"`tau`: {len(tau)}, `n`: {self.n}")
+        if len(tau) != len(y):
+            raise ValueError(
+                f"`tau` and `y` must have the same length, "
+                f"`tau`: {len(tau)}, `y`: {len(y)}"
+            )
+        B_ji = self.bsplmatrix(tau, left_n, right_n)
+
+        alpha = linalg.solve(B_ji, y[:, np.newaxis])
+        self.alpha = alpha[:, 0]
+    
+    # Evaluate the piecewise polynomial spline value for given x value
+    def ppev_single(self, x):
+        sum = 0
+        for i, alpha_ in enumerate(self.alpha):
+            sum += alpha_ * bsplev_single(x, i, self.k, self.t)
+        return sum
