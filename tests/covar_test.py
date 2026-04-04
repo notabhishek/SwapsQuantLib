21bd4ac62445381c53a7950999450e7f2709c345
Abhishek Tiwari <notabhishektiwari@gmail.com>
2026-04-04 16:45:20 +0100

    fix covar_test

--- tests/covar_test.py
+++ tests/covar_test.py
@@ -5,27 +5,22 @@ from datetime import datetime, timedelta
 from core.portfolio import Portfolio
 
 nodes = {
-    datetime(2022, 1, 1) : Dual(1, {'v0' : 1}),
-    datetime(2023, 1, 1) : Dual(1, {'v1' : 1}),
-    datetime(2024, 1, 1) : Dual(1, {'v2' : 1}),
-    datetime(2027, 1, 1) : Dual(1, {'v3' : 1}),
-    datetime(2032, 1, 1) : Dual(1, {'v4' : 1}),
+    datetime(2022, 1, 1): Dual(1, {"v0": 1}),
+    datetime(2024, 1, 1): Dual(1, {"v1": 1}),
+    datetime(2027, 1, 1): Dual(1, {"v2": 1}),
+    datetime(2032, 1, 1): Dual(1, {"v3": 1}),
+    datetime(2052, 1, 1): Dual(1, {"v4": 1}),
 }
-
-# annual 1Y, 2Y, 5Y, 10Y swaps and their par market rates
-par_swaps = {
-    Swap(datetime(2022, 1, 1), 12 * 1, 12, 12)  : 1.210,
-    Swap(datetime(2022, 1, 1), 12 * 2, 12, 12)  : 1.635,
-    Swap(datetime(2022, 1, 1), 12 * 5, 12, 12)  : 1.885,
-    Swap(datetime(2022, 1, 1), 12 * 10, 12, 12) : 1.930,
+swaps = {
+    Swap(datetime(2022, 1, 1), 12*2, 12, 12): 1.20,
+    Swap(datetime(2022, 1, 1), 12*5, 12, 12): 1.66,
+    Swap(datetime(2022, 1, 1), 12*10, 12, 12): 1.93,
+    Swap(datetime(2022, 1, 1), 12*30, 12, 12): 2.20,
 }
+s_cv = SolvedCurve(nodes=nodes, interpolation="log_linear", swaps=list(swaps.keys()), obj_rates=list(swaps.values()))
+s_cv.iterate() 
 
-curve = SolvedCurve(nodes=nodes,
-swaps=list(par_swaps.keys()),
-obj_rates=list(par_swaps.values()),
-                    interpolation='log_linear',
-                    optimization_algo='levenberg_marquardt')
-                
+print(s_cv.iterate())             
 
 swaps = [
     Swap(datetime(2022, 1, 1), 12 * 2, 12, 12, fixed_rate=1.20, notional=-50.9e6),
@@ -36,4 +31,5 @@ swaps = [
 
 portfolio = Portfolio(objects=swaps)
 
-portfolio.risk(curve)
+print(f'{portfolio.npv(s_cv).real=}')
+print(f'{portfolio.risk(s_cv).real=}')
