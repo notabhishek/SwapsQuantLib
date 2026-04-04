37ada91e4d307e7c23e91210aa5b837d853d1d48
Abhishek Tiwari <notabhishektiwari@gmail.com>
2026-04-04 16:44:11 +0100

    fix typo in Portfolio

--- core/portfolio.py
+++ core/portfolio.py
@@ -5,14 +5,14 @@ class Portfolio(Covar_):
     def __init__(self, objects: list = []):
         self.objects = objects
     
-    def risks(self, curve: SolvedCurve):
+    def risk(self, curve: SolvedCurve):
         risk = self.objects[0].risk(curve)
-        for obj in objects[1:]:
+        for obj in self.objects[1:]:
             risk += obj.risk(curve)
         return risk 
     
     def npv(self, curve: SolvedCurve):
         npv = self.objects[0].npv(curve)
-        for obj in objects[1:]:
+        for obj in self.objects[1:]:
             npv += obj.npv(curve)
         return npv
